110c2e196ff84641ffddbac708ebf55fb59855b6
Saikat Das <118115781+saikatdasdeveloper@users.noreply.github.com>
2024-06-23 22:37:10 +0530

    Update Rock Paper Scissor

--- Day 4/rock_paper.py
+++ Day 4/rock_paper.py
@@ -27,5 +27,48 @@ scissors = '''
 ---.__(___)
 '''
 
-input("Enter 0 for rock, 1 for paper, 2 for Scissors")
+user_in=input("Enter 0 for rock, 1 for paper, 2 for Scissors")
+
+random_computer_in = random.randint(0,2)
+
+if user_in == "0" :
+    print(rock)
+if user_in == "1" :
+    print(paper)
+if user_in == "2" :
+    print(scissors)
+
+if random_computer_in == 0 :
+    print(rock)
+if random_computer_in == 1 :
+    print(paper)
+if random_computer_in == 2 :
+    print(scissors)
+
+if user_in == "0" and random_computer_in == 0 :
+    print("Draw")
+
+if user_in == "0" and random_computer_in == 1 :
+    print("You Lose")
+
+if user_in == "0" and random_computer_in == 2 :
+    print("You Win")
+
+if user_in == "1" and random_computer_in == 0 :
+    print("You Win")
+
+if user_in == "1" and random_computer_in == 1 :
+    print("Draw")
+
+if user_in == "1" and random_computer_in == 2 :
+    print("You Lose")
+
+if user_in == "2" and random_computer_in == 0 :
+    print("You Lose")
+
+if user_in == "2" and random_computer_in == 1 :
+    print("You Win")
+
+if user_in == "2" and random_computer_in == 2 :
+    print("Draw")
 
