4c7c4d22fe334ce98cf74edf8e488e80f41fd14b
Saikat Das <118115781+saikatdasdeveloper@users.noreply.github.com>
2024-06-23 19:22:26 +0530

    Create Flip_Coin.py

---
+++ Day 4/Flip_Coin.py
@@ -0,0 +1,10 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+
+random_face = random.randint(0,1)
+
+if random_face == 1 :
+  print("Heads")
+else :
+  print("Tails")
