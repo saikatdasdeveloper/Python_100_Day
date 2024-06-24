900f728b183cc03a328e17179e78335ae14d8d09
Saikat Das <118115781+saikatdasdeveloper@users.noreply.github.com>
2024-06-24 21:27:23 +0530

    Update for_loop_practise.py

--- Day_5/for_loop_practise.py
+++ Day_5/for_loop_practise.py
@@ -1,6 +1,7 @@
 # Input a Python list of student heights
 student_heights = input().split()
 for n in range(0, len(student_heights)):
+### Converts string to int
   student_heights[n] = int(student_heights[n])
 # 🚨 Don't change the code above 👆
   
