e712f046d324c52f6bd88752ffd32ab754ff5750
Saikat Das <118115781+saikatdasdeveloper@users.noreply.github.com>
2024-07-01 22:11:23 +0530

    part2

---
+++ Day9/nesting_dict_list.py
@@ -0,0 +1,39 @@
+##Nesting
+
+#Nesting a list in dictionary
+
+dict_list_nst = {
+    "name": "Saikat",
+    "College": "Tripura Institute of Technology",
+    "Courses": ["Python", "Engineering", "Embeded Systems", "Web Development"]
+}
+
+print(dict_list_nst["Courses"])
+
+#Nesting a dictionary in a dictionary
+
+dict_dict_nst = {
+    "France" : {"Cities_Visited": ["Paris", "Lille"], "Visted" : 10},
+    "India" : {"Cities_Visited": ["Delhi", "Mumbai"], "Visted" : 8}
+}
+
+print(dict_dict_nst["France"]["Cities_Visited"])
+
+#Nesting a dictionary in a list
+
+list_dict_nst = [
+    {
+        "name": "Saikat",
+        "age" : "23",
+        "location" : "Bengaluru",
+        "Visited" : ["Kolkata", "Agartala, Chennai"]
+    },
+    {
+        "name": "Ankita",
+        "age" : "21",
+        "location" : "Mumbai",
+        "visited" : ["Chennai", "Noida"]
+    }
+]
+
+print(list_dict_nst[0]["location"])
