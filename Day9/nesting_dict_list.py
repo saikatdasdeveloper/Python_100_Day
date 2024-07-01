34c53a5fc4315148807a6496e5a918cf382b39ee
Saikat Das <118115781+saikatdasdeveloper@users.noreply.github.com>
2024-07-01 23:24:13 +0530

    Part 3

--- Day9/nesting_dict_list.py
+++ Day9/nesting_dict_list.py
@@ -37,3 +37,15 @@ list_dict_nst = [
 ]
 
 print(list_dict_nst[0]["location"])
+
+
+
+
+## Example
+order = {
+    "starter": {1: "Salad", 2: "Soup"},
+    "main": {1: ["Burger", "Fries"], 2: ["Steak"]},
+    "dessert": {1: ["Ice Cream"], 2: []},
+}
+
+print(order["main"][2][0])
